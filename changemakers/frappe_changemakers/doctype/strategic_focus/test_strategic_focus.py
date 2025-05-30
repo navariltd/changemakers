96c5006a7fd69d619f797e77d356778e6c75aa5d
muruthigitau <muruthigitau@gmail.com>
2025-05-30 09:52:57 +0300

    feat: Add Partner, Partner Type, Project Level, and Strategic Focus DocTypes
    - Introduced new DocTypes: Partner, Partner Type, Project Level, and Strategic Focus.
    - Updated Partner List to include "Partner" in link filters and set default value for "Partner From".

---
+++ changemakers/frappe_changemakers/doctype/strategic_focus/test_strategic_focus.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, hussain@frappe.io and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStrategicFocus(FrappeTestCase):
+	pass
