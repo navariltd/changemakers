971324f306227b6af1b507a49225cf948e719af4
symonmuchemi <simonkenya846@gmail.com>
2025-06-23 08:18:15 +0300

    update: Adds filters and columns to donation budget report
    Adds "Budget Against" and "Fiscal Year" filters to the Donation Budget Allocation Report.
    Also, defines the columns for the report.

--- changemakers/frappe_changemakers/report/donation_budget_allocation_report/donation_budget_allocation_report.js
+++ changemakers/frappe_changemakers/report/donation_budget_allocation_report/donation_budget_allocation_report.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Donation Budget Allocation Report"] = {
-	"filters": [
-
-	]
+	filters: [
+		{
+			fieldname: "budget_against",
+			label: __("Budget Against"),
+			fieldtype: "Select",
+			options: [
+				"Donor",
+				"Project",
+				"Employee",
+				"Task",
+				"Cost Center",
+				"Program",
+			],
+		},
+		{
+			fieldname: "fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			default: frappe.defaults.get_user_default("fiscal_year"),
+		},
+	],
 };
--- changemakers/frappe_changemakers/report/donation_budget_allocation_report/donation_budget_allocation_report.py
+++ changemakers/frappe_changemakers/report/donation_budget_allocation_report/donation_budget_allocation_report.py
@@ -1,9 +1,65 @@
 # Copyright (c) 2025, hussain@frappe.io and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns, data = [], []
+    return columns, data
+
+
+def get_columns():
+    return [
+        {"fieldname": "name", "fieldtype": "Data", "label": "Name", "width": 150},
+        {
+            "fieldname": "budget_against",
+            "fieldtype": "Data",
+            "label": "Budget Against",
+            "options": "Project",
+            "width": 200,
+        },
+        {
+            "fieldname": "donor",
+            "fieldtype": "Link",
+            "label": "Donor",
+            "options": "Donor",
+            "width": 200,
+        },
+        {
+            "fieldname": "donation",
+            "fieldtype": "Link",
+            "label": "Donation",
+            "options": "Donation",
+            "width": 200,
+        },
+        {
+            "fieldname": "amount",
+            "fieldtype": "Currency",
+            "label": "Amount",
+        },
+        {
+            "fieldname": "budget_account",
+            "fieldtype": "Link",
+            "label": "Budget Account",
+            "options": "Budget Account",
+        },
+        {
+            "fieldname": "budget_amount",
+            "fieldtype": "Currency",
+            "label": "Allocation",
+            "width": 150,
+        },
+        {
+            "fieldname": "months_distributed",
+            "fieldtype": "Int",
+            "label": "Months Distributed",
+            "width": 150,
+        },
+        {
+            "fieldname": "percentage",
+            "fieldtype": "Percent",
+            "label": "Percentage",
+            "width": 100,
+        },
+    ]
