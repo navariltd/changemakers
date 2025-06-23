3b9a9e1473b2c3d970a07c57c86b65bc29dd47ed
symonmuchemi <simonkenya846@gmail.com>
2025-06-23 07:44:38 +0300

    Update: Changes report type to Script Report
    Updates the report type from Report Builder to Script Report.

---
+++ changemakers/frappe_changemakers/report/donation_budget_allocation_report/donation_budget_allocation_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, hussain@frappe.io and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Donation Budget Allocation Report"] = {
+	"filters": [
+
+	]
+};
