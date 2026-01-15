2cd4e8bd0cb4957f0be14f4852ef0a30f6f247bc
muruthigitau <muruthigitau@gmail.com>
2026-01-15 12:15:44 +0300

    Add Donation Allocation Criteria and Criteria Type doctypes

---
+++ changemakers/frappe_changemakers/doctype/donation_allocation_criteria_type/test_donation_allocation_criteria_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, hussain@frappe.io and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDonationAllocationCriteriaType(FrappeTestCase):
+	pass
