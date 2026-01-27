76428d3d10eeb189ca29ea7ccea17132e34b01ec
muruthigitau <muruthigitau@gmail.com>
2026-01-27 17:08:39 +0300

    feat: rename beneficiary doctype and add disbursement fields
    Rename Donation Allocation Beneficiary to Beneficiary Disbursement
    Entry Party to better reflect disbursement semantics.
    
    Key changes:
    - Renamed the beneficiary doctype.
    - Added itemized fields (item, qty, rate) and mode of payment.
    - Made amount field read-only.
    - Added a minimal Document class.
    - Updated client script references.
    - Updated unit test names to match the new doctype.

--- changemakers/frappe_changemakers/doctype/beneficiary_disbursement_entry_party/beneficiary_disbursement_entry_party.js
+++ changemakers/frappe_changemakers/doctype/beneficiary_disbursement_entry_party/beneficiary_disbursement_entry_party.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, hussain@frappe.io and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Donation Allocation Beneficiary", {
+// frappe.ui.form.on("Beneficiary Disbursement Entry Party", {
 // 	refresh(frm) {
 
 // 	},
--- changemakers/frappe_changemakers/doctype/beneficiary_disbursement_entry_party/test_beneficiary_disbursement_entry_party.py
+++ changemakers/frappe_changemakers/doctype/beneficiary_disbursement_entry_party/test_beneficiary_disbursement_entry_party.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestDonationAllocationBeneficiary(FrappeTestCase):
+class TestBeneficiaryDisbursementEntryParty(FrappeTestCase):
 	pass
